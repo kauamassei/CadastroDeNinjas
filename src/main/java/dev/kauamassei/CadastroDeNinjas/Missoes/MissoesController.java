4ee551d783c22ce307bff3a2d1c66acefae3ee0f
kauamassei <gameplayultra11@gmail.com>
2026-07-26 00:31:23 -0300

    feat: Coloca lombok para abstracão

---
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,16 @@
+package dev.kauamassei.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MissoesController {
+
+    @GetMapping("/missoes")
+    public String missoes() {
+        return "testando missoes";
+    }
+
+}
