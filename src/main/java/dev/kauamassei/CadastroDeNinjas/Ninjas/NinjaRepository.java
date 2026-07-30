39efae57dfb5d9cc0b08110b0b9aa1f4c3356b4e
kauamassei <gameplayultra11@gmail.com>
2026-07-29 23:24:37 -0300

    build: conecta o banco de dados H2 à minha app Spring

--- src/main/java/dev/kauamassei/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.kauamassei.CadastroDeNinjas.Ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
