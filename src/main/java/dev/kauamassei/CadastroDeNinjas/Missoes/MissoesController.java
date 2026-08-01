d9739fd3561571a70cf796723fc7282b7cb188f6
kauamassei <gameplayultra11@gmail.com>
2026-08-01 19:55:58 -0300

    feat: Criar as rotas do Controller com mock

--- src/main/java/dev/kauamassei/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,18 +1,30 @@
 package dev.kauamassei.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
-    @GetMapping("/missoes")
-    public String missoes() {
-        return "testando missoes";
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missoes listadas com sucesso";
     }
 
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missoes listadas com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missoes listadas com sucesso";
+    }
 
 
 }
