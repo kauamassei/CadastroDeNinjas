3c08cc40f27ba876a91a544681b1acfeb54cde71
kauamassei <gameplayultra11@gmail.com>
2026-08-01 19:44:49 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/kauamassei/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.kauamassei.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,33 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    // Adicionar ninja
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os ninjas
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar ninja";
+    }
+
+    // Mostrar ninja por ID
+    @GetMapping("/todosID")
+    public String mostrarNinjaPorId() {
+        return "Ninja ID";
+    }
+    // Atualizar dados do ninja
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar ninja por ID";
+    }
+    // Deletar ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja criado";
     }
 }
