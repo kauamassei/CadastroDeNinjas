294e808e4a88a2cf0206f2da61fc7fb94d3b0dae
kauamassei <gameplayultra11@gmail.com>
2026-08-01 02:39:48 -0300

    feat: Migrations para tb_cadastro incluindo a column de rank

--- src/main/java/dev/kauamassei/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -8,6 +8,8 @@ public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+
+
 	}
 
 }
--- src/main/java/dev/kauamassei/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/Missoes/MissoesController.java
@@ -13,4 +13,6 @@ public class MissoesController {
         return "testando missoes";
     }
 
+
+
 }
