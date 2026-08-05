c24a1ab2c92d1153d889ccbdbb9bc73d40949cae
kauamassei <gameplayultra11@gmail.com>
2026-08-05 02:01:43 -0300

    feat: Inclui DTO como abstracao para minha entidade na camada de service e controller

--- src/main/java/dev/kauamassei/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/kauamassei/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -10,9 +10,11 @@ public class NinjaService {
 
     // injecao de dependencias
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     // lista os ninjas
@@ -27,8 +29,10 @@ public class NinjaService {
     }
 
     // Cria ninja
-    public NinjaModel criarNinja(NinjaModel ninja) {
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO) {
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     // Deleta ninja
